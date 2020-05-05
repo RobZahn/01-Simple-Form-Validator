f49b3311d810e77444b85d04f1f17e572dfc02de
Robert Zahn <rob.jzahn@gmail.com>
2020-05-05 10:11:38 -0400

    Add check length

--- dist/script.js
+++ dist/script.js
@@ -38,6 +38,25 @@ function checkRequired(inputArr) {
   });
 }
 
+// Check input length
+function checkLength(input, min, max) {
+  const length = input.value.length;
+
+  if (length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters.`
+    );
+  } else if (length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters.`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
 // Get field name
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -49,4 +68,6 @@ form.addEventListener('submit', e => {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
 });
