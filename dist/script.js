814294ddc30ab8518ed18cf72ec716656c58245f
Robert Zahn <rob.jzahn@gmail.com>
2020-05-05 10:15:48 -0400

    Add check email

--- dist/script.js
+++ dist/script.js
@@ -22,9 +22,14 @@ function showSuccess(input) {
 }
 
 // Check for valid email
-function isValidEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+function checkEmail(input) {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regex.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `Please enter a valid email address.`);
+  }
 }
 
 // Check required fields
@@ -70,4 +75,5 @@ form.addEventListener('submit', e => {
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
+  checkEmail(email);
 });
