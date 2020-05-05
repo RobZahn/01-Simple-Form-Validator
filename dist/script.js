9e1601191cb80275c9ca61af71c452dbcee23747
Robert Zahn <rob.jzahn@gmail.com>
2020-05-05 09:08:20 -0400

    Add show input error message function

--- dist/script.js
+++ dist/script.js
@@ -4,6 +4,24 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Show Input Error Message
+
+function showError(input, msg) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+
+  const small = formControl.querySelector('small');
+  small.innerText = msg;
+}
+
+// Event Listeners
+
 form.addEventListener('submit', e => {
   e.preventDefault();
+
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
 });
